4fbabb48bd4d9b4585b7ba787c08f33b242ec5c9
Ellensramalho <ellen_santos_bsf@hotmail.com>
2026-02-19 16:55:26 -0300

    Footer estilizado

---
+++ Footer/index.jsx
@@ -0,0 +1,19 @@
+export function Footer(){
+  return (
+    <footer>
+    <ul>
+    <li><a href="#"> Endereço</a></li>
+    <li>Telefone: 99887766</li>
+    </ul>
+     <ul>
+    <li><a href="#"> Instagram</a></li>
+    <li>Brasil</li>
+    </ul>
+     <ul>
+    <li><a href="#"> Whatsapp</a></li>
+    <li>Site Compras</li>
+    </ul>
+    <p>©Todos os direitos reservados.</p>
+    </footer>
+  )
+}
